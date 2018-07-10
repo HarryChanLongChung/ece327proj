a08d444d39fceb7dc0fa7e5a2807c1f13da6bb0e
Long Chung Chan <longchungchan@Longs-MacBook-Pro.local>
2018-07-10 12:37:49 -0400

    init file

---
+++ kirsch.vhd
@@ -0,0 +1,28 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.util.all;
+use work.kirsch_synth_pkg.all;
+
+entity kirsch is
+  port(
+    clk        : in  std_logic;                      
+    reset      : in  std_logic;                      
+    i_valid    : in  std_logic;                 
+    i_pixel    : in  unsigned(7 downto 0);
+    o_valid    : out std_logic;                 
+    o_edge     : out std_logic;	                     
+    o_dir      : out direction_ty;
+    o_mode     : out mode_ty;
+    o_row      : out unsigned(7 downto 0);
+    o_col      : out unsigned(7 downto 0);
+  );  
+end entity;
+
+
+architecture main of kirsch is
+begin  
+  
+end architecture;
